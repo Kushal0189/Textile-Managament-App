6a9c315fff697a27147e3d50837ed5cbb8ae00d7
Harish <harishjarda23@gmail.com>
2019-05-22 14:43:39 +0530

    sucess

---
+++ app/src/main/java/com/example/myapplication/DashboardActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class DashboardActivity extends AppCompatActivity {
+    private SessionHandler session;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        session = new SessionHandler(getApplicationContext());
+        User user = session.getUserDetails();
+        TextView welcomeText = findViewById(R.id.welcomeText);
+
+        welcomeText.setText("Welcome "+user.getFullName()+", your session will expire on "+user.getSessionExpiryDate());
+
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -0,0 +1,178 @@
+package com.example.myapplication;
+
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RegisterActivity extends AppCompatActivity {
+    private static final String KEY_STATUS = "status";
+    private static final String KEY_MESSAGE = "message";
+    private static final String KEY_FULL_NAME = "full_name";
+    private static final String KEY_USERNAME = "username";
+    private static final String KEY_PASSWORD = "password";
+    private static final String KEY_EMPTY = "";
+    private EditText etEmailID;
+    private EditText etPassword;
+    private EditText etFullName;
+    private String emailid;
+    private String password;
+    private String fullName;
+    private ProgressDialog pDialog;
+    private String register_url = "https://192.168.31.252:8888/api/member/register.php";
+    private SessionHandler session;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        session = new SessionHandler(getApplicationContext());
+        setContentView(R.layout.activity_register);
+
+        etEmailID = findViewById(R.id.etemail);
+        etPassword = findViewById(R.id.etPassword);
+        etFullName = findViewById(R.id.etFullName);
+
+        Button login = findViewById(R.id.btnRegisterLogin);
+        Button register = findViewById(R.id.btnRegister);
+
+        //Launch Login screen when Login Button is clicked
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Retrieve the data entered in the edit texts
+                emailid = etEmailID.getText().toString().toLowerCase().trim();
+                password = etPassword.getText().toString().trim();
+                if (validateInputs()) {
+                    registerUser();
+                }
+
+            }
+        });
+
+    }
+
+    /**
+     * Display Progress bar while registering
+     */
+    private void displayLoader() {
+        pDialog = new ProgressDialog(RegisterActivity.this);
+        pDialog.setMessage("Signing Up.. Please wait...");
+        pDialog.setIndeterminate(false);
+        pDialog.setCancelable(false);
+        pDialog.show();
+
+    }
+
+    /**
+     * Launch Dashboard Activity on Successful Sign Up
+     */
+    private void loadDashboard() {
+        Intent i = new Intent(getApplicationContext(), DashboardActivity.class);
+        startActivity(i);
+        finish();
+
+    }
+
+    private void registerUser() {
+        displayLoader();
+        JSONObject request = new JSONObject();
+        try {
+            //Populate the request parameters
+            request.put(KEY_USERNAME, emailid);
+            request.put(KEY_PASSWORD, password);
+            request.put(KEY_FULL_NAME, fullName);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest jsArrayRequest = new JsonObjectRequest
+                (Request.Method.POST, register_url, request, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        pDialog.dismiss();
+                        try {
+                            //Check if user got registered successfully
+                            if (response.getInt(KEY_STATUS) == 0) {
+                                //Set the user session
+                                session.loginUser(emailid,fullName);
+                                loadDashboard();
+
+                            }else if(response.getInt(KEY_STATUS) == 1){
+                                //Display error message if username is already existsing
+                                etEmailID.setError("Username already taken!");
+                                etEmailID.requestFocus();
+
+                            }else{
+                                Toast.makeText(getApplicationContext(),
+                                        response.getString(KEY_MESSAGE), Toast.LENGTH_SHORT).show();
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        pDialog.dismiss();
+
+                        //Display error message whenever an error occurs
+                        Toast.makeText(getApplicationContext(),
+                                error.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsArrayRequest);
+    }
+
+    /**
+     * Validates inputs and shows error if any
+     * @return
+     */
+    private boolean validateInputs() {
+        if (KEY_EMPTY.equals(fullName)) {
+            etFullName.setError("Full Name cannot be empty");
+            etFullName.requestFocus();
+            return false;
+
+        }
+        if (KEY_EMPTY.equals(emailid)) {
+            etEmailID.setError("Username cannot be empty");
+            etEmailID.requestFocus();
+            return false;
+        }
+        if (KEY_EMPTY.equals(password)) {
+            etPassword.setError("Password cannot be empty");
+            etPassword.requestFocus();
+            return false;
+        }
+
+
+        return true;
+    }
+}
